24f588d9187ad11b4eb7060b222f306eb3d42f19
Rohithossain12 <https://github.com/Rohithossain12>
2024-12-29 22:05:58 +0600

    auth related apis create

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uv360.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -27,6 +35,18 @@ async function run() {
 
     const foodCollection = client.db("foodDB").collection("food");
     const purchaseCollection = client.db("foodDB").collection("purchases");
+
+    // auth related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "5h" });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
 
     // get all food items
     app.get("/foodItems", async (req, res) => {
